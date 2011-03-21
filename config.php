7506de2bb4b6424946baca0400ad97a0f4aa6c29
Chris <cduncan@delorum.com>
2011-03-21 09:41:52 -0600

    Updated config

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+// phpBB 3.0.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'mysql';
+$dbhost = 'localhost:/tmp/mysql/tinisha.sock';
+$dbport = '';
+$dbname = 'tinisha';
+$dbuser = 'daria';
+$dbpasswd = 'x6J73CDE';
+$table_prefix = 'phpbb_';
+$acm_type = 'file';
+$load_extensions = '';
+
+@define('PHPBB_INSTALLED', true);
+// @define('DEBUG', true);
+// @define('DEBUG_EXTRA', true);
+?>
